58e2342e2638e87bf5e7145e42f9a32972674aa0
quemerotw <quemerotw@localhost.localdomain>
2025-03-08 17:45:46 -0300

    comit inicial

---
+++ app.js
@@ -0,0 +1,50 @@
+// Funcionalidad de scroll suave en el menú
+document.querySelectorAll('.navbar a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+// Funcionalidad de slider
+const sliders = document.querySelectorAll('.slider');
+sliders.forEach(slider => {
+  const container = slider.querySelector('.slider-container');
+  const slides = slider.querySelectorAll('.slide');
+  const prevBtn = slider.querySelector('.prev');
+  const nextBtn = slider.querySelector('.next');
+  
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    container.style.transform = `translateX(${-index * 100}%)`;
+  }
+
+  prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
+    showSlide(currentIndex);
+  });
+
+  nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
+    showSlide(currentIndex);
+  });
+  console.log("asdd");
+  
+});
+
+// Funcionalidad del menú hamburguesa en móviles
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
